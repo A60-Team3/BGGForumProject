cd94fb1b6ff45b8454bd6cbcd96f575d62c56680
Milan Statev <milan_statev@yahoo.co.uk>
2024-07-18 20:25:24 +0300

    Initial commit

---
+++ src/main/java/com/example/bggforumproject/BggForumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bggforumproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BggForumProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BggForumProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bggforumproject/BggForumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bggforumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BggForumProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
