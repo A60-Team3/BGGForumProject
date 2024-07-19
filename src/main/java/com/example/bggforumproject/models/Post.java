cc9f3fbbd23428860a147f38cdb6414fb33d850a
asenmomchev <asen.momchev.a60@learn.telerikacademy.com>
2024-07-19 16:08:33 +0300

    Add Post POJO

---
+++ src/main/java/com/example/bggforumproject/models/Post.java
@@ -0,0 +1,55 @@
+package com.example.bggforumproject.models;
+
+import java.time.LocalDateTime;
+
+public class Post {
+
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private User userId;
+
+    public Post() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+}
