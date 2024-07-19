b842ec42eca6ec5c5312ad643bcd66a85adebcb6
Milan Statev <milan_statev@yahoo.co.uk>
2024-07-19 13:44:48 +0300

    Change DB build logic

--- db/create.sql
+++ db/create.sql
@@ -1,50 +1,36 @@
+DROP DATABASE boardgames_forum;
+
 CREATE DATABASE boardgames_forum;
 USE boardgames_forum;
 
 CREATE TABLE users
 (
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    first_name   VARCHAR(32) NOT NULL,
-    last_name    VARCHAR(32) NOT NULL,
-    email        VARCHAR(50) NOT NULL UNIQUE,
-    username     VARCHAR(50) NOT NULL,
-    password     VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(20),
-    registered_at DATETIME DEFAULT NOW(),
-    user_role    ENUM ('ADMIN', 'MODERATOR', 'USER'),
-    is_Blocked   tinyint DEFAULT 0,
-    is_Deleted   tinyint DEFAULT 0
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    first_name    VARCHAR(32)                         NOT NULL,
+    last_name     VARCHAR(32)                         NOT NULL,
+    email         VARCHAR(50)                         NOT NULL UNIQUE,
+    username      VARCHAR(50)                         NOT NULL,
+    password      VARCHAR(50)                         NOT NULL,
+    phone_number  VARCHAR(20),
+    registered_at DATETIME DEFAULT current_timestamp(),
+    user_role     ENUM ('ADMIN', 'MODERATOR', 'USER') NOT NULL,
+    is_blocked    tinyint  DEFAULT 0,
+    is_deleted    tinyint  DEFAULT 0
 );
 
 CREATE TABLE posts
 (
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    title    VARCHAR(64)   NOT NULL,
-    content  VARCHAR(8192) NOT NULL,
-    likes    INT DEFAULT 0,
-    dislikes INT DEFAULT 0,
-    created_at DATETIME DEFAULT NOW(),
-    user_id  INT           NOT NULL,
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    title      VARCHAR(64),
+    content    VARCHAR(8192) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    user_id    INT           NOT NULL,
+    comment_to INT,
 
     constraint fk_posts_users
-        foreign key (user_id) references users (id)
-);
-
-CREATE TABLE comments
-(
-    id    INT AUTO_INCREMENT PRIMARY KEY ,
-    content  VARCHAR(8192) NOT NULL,
-    likes    INT DEFAULT 0,
-    dislikes INT DEFAULT 0,
-    created_at DATETIME DEFAULT NOW(),
-    user_id  INT           NOT NULL,
-    post_id INT NOT NULL,
-    replying_to INT,
-
-    constraint fk_comments_posts
-        foreign key (post_id) references posts (id),
+        foreign key (user_id) references users (id),
     constraint fk_comments_comments
-        foreign key (replying_to) references comments (id)
+        foreign key (comment_to) references posts (id)
 );
 
 CREATE TABLE tags
@@ -64,5 +50,14 @@ CREATE TABLE posts_tags
 );
 
 
-
-
+CREATE TABLE reactions
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    post_id       INT                      NOT NULL,
+    user_id       INT                      NOT NULL,
+    reaction_type ENUM ('LIKE', 'DISLIKE') NOT NULL,
+    constraint fk_reactions_posts
+        foreign key (post_id) references posts (id),
+    constraint fk_reactions_users
+        foreign key (user_id) references users (id)
+);
