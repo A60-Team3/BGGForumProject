2e8868a49bea1c6c4ed6c0f1c60cc4b283f892f6
asenmomchev <asen.momchev.a60@learn.telerikacademy.com>
2024-07-29 09:55:06 +0300

    Add tеsts for PostService

---
+++ src/test/java/com/example/bggforumproject/services/PostServiceTests.java
@@ -0,0 +1,195 @@
+package com.example.bggforumproject.services;
+
+import com.example.bggforumproject.exceptions.AuthorizationException;
+import com.example.bggforumproject.helpers.filters.PostFilterOptions;
+import com.example.bggforumproject.models.Post;
+import com.example.bggforumproject.models.User;
+import com.example.bggforumproject.repositories.contracts.PostRepository;
+import com.example.bggforumproject.service.PostServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.example.bggforumproject.helpers.CreationHelper.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTests {
+
+    @InjectMocks
+    private PostServiceImpl postService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Test
+    public void get_Should_CallRepository(){
+        PostFilterOptions mockPostFilterOptions = createMockPostFilterOptions();
+
+        Mockito.when(postRepository.get(mockPostFilterOptions))
+                .thenReturn(null);
+
+        postService.get(mockPostFilterOptions);
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .get(mockPostFilterOptions);
+    }
+
+    @Test
+    public void get_Should_CallRepository_When_IdIsProvided(){
+        postService.get(1);
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .get(1);
+    }
+
+    @Test
+    public void get_Should_CallRepository_When_TitleIsProvided(){
+        postService.get(Mockito.anyString());
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .get(Mockito.anyString());
+    }
+
+    @Test
+    public void get_Should_ReturnPost_When_IdIsProvided(){
+        Post post = createMockPost();
+
+        Mockito.when(postRepository.get(post.getId()))
+                .thenReturn(post);
+
+        Post result = postService.get(post.getId());
+
+        Assertions.assertSame(post, result);
+    }
+
+    @Test
+    public void get_Should_ReturnPost_When_TitleIsProvided(){
+        Post post = createMockPost();
+
+        Mockito.when(postRepository.get(post.getTitle()))
+                .thenReturn(post);
+
+        Post result = postService.get(post.getTitle());
+
+        Assertions.assertSame(post, result);
+    }
+
+    @Test
+    public void getMostCommented_Should_CallRepository(){
+        postService.getMostCommented();
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .getMostCommented();
+    }
+
+    @Test
+    public void getMostRecentlyCreated_Should_CallRepository(){
+        postService.getMostRecentlyCreated();
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .getMostRecentlyCreated();
+    }
+
+    @Test
+    public void create_Should_CreatePost_When_Valid(){
+        Post post = createMockPost();
+        User user = createMockUser();
+
+        postService.create(post, user);
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .create(Mockito.any(Post.class));
+    }
+
+    @Test
+    public void update_Should_UpdatePost_When_UserIsCreator(){
+        Post post = createMockPost();
+        User user = createMockUser();
+
+        post.setUserId(user);
+
+        Mockito.when(postRepository.get(1)).thenReturn(post);
+
+        postService.update(post, user);
+        Mockito.verify(postRepository, Mockito.times(1))
+                .update(post);
+    }
+
+    @Test
+    public void update_Should_Throw_When_UserIsNotCreator(){
+        Post post = createMockPost();
+        User user = createMockUser();
+        User notCreator = createMockUser();
+        notCreator.setId(2);
+        notCreator.setUsername("notcreator");
+        post.setUserId(user);
+
+        Mockito.when(postRepository.get(1)).thenReturn(post);
+
+        Assertions.assertThrows(AuthorizationException.class,
+                () -> postService.update(post, notCreator));
+    }
+
+    @Test
+    public void delete_Should_DeletePost_When_UserIsCreator(){
+        Post post = createMockPost();
+        User user = createMockUser();
+        post.setUserId(user);
+
+        Mockito.when(postRepository.get(1)).thenReturn(post);
+
+        postService.delete(post.getId(), user);
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .delete(post.getId());
+    }
+
+    @Test
+    public void delete_Should_DeletePost_When_UserIsAdmin(){
+        Post post = createMockPost();
+        User user = createMockUser();
+        post.setUserId(user);
+        User admin = createMockAdmin();
+
+        Mockito.when(postRepository.get(1)).thenReturn(post);
+
+        postService.delete(post.getId(), admin);
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .delete(post.getId());
+    }
+
+    @Test
+    public void delete_Should_DeletePost_When_UserIsModerator(){
+        Post post = createMockPost();
+        User user = createMockUser();
+        post.setUserId(user);
+        User moderator = createMockModerator();
+
+        Mockito.when(postRepository.get(1)).thenReturn(post);
+
+        postService.delete(post.getId(), moderator);
+
+        Mockito.verify(postRepository, Mockito.times(1))
+                .delete(post.getId());
+    }
+
+    @Test
+    public void delete_Should_Throw_When_UserIsNotOwnerOrAdminOrModerator(){
+        Post post = createMockPost();
+        User user = createMockUser();
+        User notCreator = createMockUser();
+        notCreator.setId(2);
+        notCreator.setUsername("notcreator");
+        post.setUserId(user);
+
+        Mockito.when(postRepository.get(1)).thenReturn(post);
+
+        Assertions.assertThrows(AuthorizationException.class,
+                () -> postService.delete(post.getId(), notCreator));
+    }
+}
