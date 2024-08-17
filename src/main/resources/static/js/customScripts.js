2f3282f278fad5c12f1a0933c743cc3cee7c370d
Milan Statev <milan_statev@yahoo.co.uk>
2024-08-17 18:59:51 +0300

    Add styling, Format some code

--- src/main/resources/static/js/customScripts.js
+++ src/main/resources/static/js/customScripts.js
@@ -1,9 +1,8 @@
 function logout() {
     const form = document.createElement('form');
     form.method = 'POST';
-    form.action = '/auth/logout';  // Make sure this matches your logout URL
+    form.action = '/auth/logout';
 
-    // CSRF token if required
     const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
     const csrfInput = document.createElement('input');
     csrfInput.type = 'hidden';
@@ -18,3 +17,19 @@ function logout() {
 function uploadPhoto(){
     document.getElementById('profile-form').submit();
 }
+
+function getRandomPosition() {
+    const randomX = Math.floor(Math.random() * 100);
+    const randomY = Math.floor(Math.random() * 100);
+    return `${randomX}% ${randomY}%`;
+}
+
+function setRandomBackgroundPosition() {
+    const element = document.getElementById('background-images');
+    element.style.backgroundPosition = getRandomPosition();
+}
+
+setRandomBackgroundPosition();
+
+// Optionally, set random position every few seconds
+setInterval(setRandomBackgroundPosition, 10000); // 10 seconds
